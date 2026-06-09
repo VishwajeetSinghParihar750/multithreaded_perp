803fca19a42a9ea44f477b589c386cb91e5e5547
Vishwajeet Singh Parihar <svishwajeet750@gmail.com>
2026-06-09 17:33:56 +0000

    spsc q

--- engine/include/perp/util/spscQueue.h
+++ engine/include/perp/util/spscQueue.h
@@ -3,25 +3,52 @@
 #include <queue>
 #include <optional>
 #include <string>
+#include <mutex>
+#include <condition_variable>
 
 template <typename T>
 class spscQueue
 {
     std::queue<T> q;
 
-    //
+    std::mutex mtx;
+    std::condition_variable cv;
+
 public:
     T waitPop()
     {
-        //
+        mtx.lock();
+
+        cv.wait(mtx, [&]() -> bool
+                { return !q.empty(); });
+
+        auto toReturn = std::move(q.front());
+        q.pop();
+
+        mtx.unlock();
+
+        return toReturn;
     }
 
     std::optional<T> pop()
     {
+        mtx.lock();
+
+        if (q.empty())
+            return std::nullopt;
+
+        auto toReturn = std::move(q.front());
+        q.pop();
+
+        mtx.unlock();
+
+        return toReturn;
     }
 
     void push(T item)
     {
-        //
+        mtx.lock();
+        q.push(std::move(item));
+        mtx.unlock();
     }
 };
